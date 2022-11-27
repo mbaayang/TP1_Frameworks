aa4545484291fccdbf095ce507d13cd0a17bc3b1
Mbayang Gueye <mbayangueye07@gmail.com>
2022-11-27 20:10:36 +0000

    back

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb+srv://mbayang:mbayang07@cluster0.tzug7mq.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res) => {
+    res.json({ message: 'Votre requete a bien ete reçu'});
+});
+
+module.exports = app;
